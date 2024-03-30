661c2bb15200549ecbe7ed146922a9da54a10bcb
Bijaya <bjskypee@gmail.com>
2024-03-30 10:50:17 -0500

    Display the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import { GameCardContainer } from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const Skeletons = [1, 2, 3, 4];
+  const Skeletons = [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15];
 
   return (
     <>
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           Skeletons.map((Skeleton) => (
